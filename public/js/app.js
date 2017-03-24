6b08cc2546625e8370b9d8590c6a0e1ca7f0a713
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-03-24 15:35:34 +0000

    Changing tweet to blog - seem to have switched words

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,17 @@ $(start);
 
 function start(){
 
-  $('#new-tweet-input').on('keyup', function(){
-    $('.tweet-counter').html(150 - $('textarea').val().length);
+  $('#new-blog-input').on('keyup', function(){
+    $('.blog-counter').html(150 - $('textarea').val().length);
     if ($('textarea').val().length > 150){
-      $('.tweet-counter').css('color', '#cc8787');
+      $('.blog-counter').css('color', '#cc8787');
     } else {
-      $('.tweet-counter').css('color', '#8899a6');
+      $('.blog-counter').css('color', '#8899a6');
     }
   });
 
-  var $tweetForm = $('#new-tweet-form');
-  $tweetForm.on('submit', function(e){
+  var $blogForm = $('#new-blog-form');
+  $blogForm.on('submit', function(e){
     e.preventDefault();
     var text = $('textarea').val();
     if (text.length > 0 && text.length < 150){
@@ -23,15 +23,15 @@ function start(){
       })
       .done(resp => {
         var $ol = $('.stream-items');
-        var tweetToAdd = '<li class="stream-item"><div class="tweet"><a href="#"><img src="http://pix.iemoji.com/images/emoji/apple/ios-9/256/white-woman.png" alt="User image goes here."></a><div class="content"><strong class="fullname">' + resp.blog.fullName + '</strong><span>&rlm;</span><span>@</span><b>' + resp.blog.screenName + '</b>&nbsp;&middot;&nbsp;<small class="time timeago">' + resp.blog.createdAt + '</small><p>' + resp.blog.blogText +   '<form action="/blogs/<%= blog._id %>" method="post"> <input type="hidden" name="_method" value="delete">         <button class="deleteButton"> Delete </button> </form></p></div></div></li>';
-        $ol.prepend(tweetToAdd);
+        var blogToAdd = '<li class="stream-item"><div class="blog"><a href="#"><img src="http://pix.iemoji.com/images/emoji/apple/ios-9/256/white-woman.png" alt="User image goes here."></a><div class="content"><strong class="fullname">' + resp.blog.fullName + '</strong><span>&rlm;</span><span>@</span><b>' + resp.blog.screenName + '</b>&nbsp;&middot;&nbsp;<small class="time timeago">' + resp.blog.createdAt + '</small><p>' + resp.blog.blogText +   '<form action="/blogs/<%= blog._id %>" method="post"> <input type="hidden" name="_method" value="delete">         <button class="deleteButton"> Delete </button> </form></p></div></div></li>';
+        $ol.prepend(blogToAdd);
 
 
         // Reset the form
         $('#fullName').val('');
         $('#screenName').val('');
         $('textarea').val('');
-        $('.tweet-counter').html(140 - $('textarea').val().length);
+        $('.blog-counter').html(140 - $('textarea').val().length);
 
       })
       .fail(data => {
