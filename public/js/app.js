cdf23f3984a7f01771a39cde1d0af3b7c029887c
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-03-26 10:51:03 +0100

    Removed console.logs

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,6 @@ function start(){
 
     $.get('http://localhost:3000/blogs')
     .done(data => {
-      console.log('DATA FROM THE GET CALL IN INDEX', data);
       $.each(data, (index, blog) => {
         var blogToAdd = '<li class="stream-item"><div class="blog"><img src="http://pix.iemoji.com/images/emoji/apple/ios-9/256/white-woman.png" alt="User image goes here."><div class="content"><strong class="fullname">' + blog.fullName + '</strong><span>&rlm;</span><span>@</span><b>' + blog.screenName + '</b>&nbsp;&middot;&nbsp;<small class="time timeago">' + $.timeago(blog.createdAt) + '</small><p>' + blog.blogText + '<button class="deleteButton" data-id="' + blog._id + '"> Delete </button></p></div></div></li>';
         $('.stream-items').prepend(blogToAdd);
